52b1336caea6b58086226c315893ac1417a2acd7
KristianJetu <124069133+KristianJetu@users.noreply.github.com>
2026-07-02 11:27:03 +0000

    Add guest user host/host

--- middleware.ts
+++ middleware.ts
@@ -7,13 +7,18 @@ export function middleware(req: NextRequest) {
   }
 
   const auth = req.headers.get("authorization");
-  const validAuth =
-    "Basic " +
-    Buffer.from(
-      `${process.env.BASIC_AUTH_USER}:${process.env.BASIC_AUTH_PASS}`
-    ).toString("base64");
 
-  if (auth !== validAuth) {
+  const users = [
+    { user: process.env.BASIC_AUTH_USER, pass: process.env.BASIC_AUTH_PASS },
+    { user: "host", pass: "host" },
+  ];
+
+  const isValid = users.some(
+    ({ user, pass }) =>
+      auth === "Basic " + Buffer.from(`${user}:${pass}`).toString("base64")
+  );
+
+  if (!isValid) {
     return new NextResponse("Přístup odepřen", {
       status: 401,
       headers: { "WWW-Authenticate": 'Basic realm="Equity Dashboard"' },
